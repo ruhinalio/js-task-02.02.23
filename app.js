4288a105c251104af96a4876a1c281a5e47ca675
ruhinalio <116165822+ruhinalio@users.noreply.github.com>
2023-02-03 14:59:45 +0400

    last comit

---
+++ app.js
@@ -0,0 +1,38 @@
+// !ici bos olan kvadrat
+// let col = 5;
+// let row = 4;
+// let result = "";
+// for (let i = 0; i < col; i++) {
+//   for (let j = 0; j < row; j++) {
+//     if (i === 0 || i === col - 1 || j === 0 || j === row - 1) {
+//       result += "* ";
+//     } else {
+//       result += "  ";
+//     }
+//   }
+//   result += "\n";
+// }
+// console.log(result);
+
+// !'n'e qeder yazdirmaq
+// var a = +prompt("ededi daxil edin");
+// for (var i = 1; i <= a; i++) {
+//   let b = ''
+//   for (let k = 1; k <= i; k++) {
+//      b+=k
+//   }
+//   console.log(b)
+// }
+
+//!tesden yazdirmaq
+// var a= prompt('ededi daxil edin')
+// var b= prompt('ededi daxil edin')
+// console.log(b,a)
+// !ferqli olan ededi tapin
+// var a= prompt('ededi daxil edin')
+// var b= prompt('ededi daxil edin')
+// for(var i=0; i<b; i++){
+//     if(a[i]!==b[i]){
+//         console.log(`${b[i]} ferlidir`);
+//     }
+// }
